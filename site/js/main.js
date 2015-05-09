62cbe1a80c0d07becd69e7443a4e926fbef068aa
buschidos <sergey.snjozyk@gmail.com>
2015-05-10 00:38:50 +0300

    canvas update

--- site/js/main.js
+++ site/js/main.js
@@ -24885,7 +24885,7 @@ var $     = require("./../../../bower_components/jquery/dist/jquery.js");
 var Morph = function(canvasObj) {
 
     this.canvas = canvasObj[0];
-    this.dur    = 500;
+    this.dur    = 400;
     this.state  = 'circle'; // can be circle, square or triangle
 
 };
@@ -24914,7 +24914,7 @@ Morph.prototype._initContent = function() {
                     destination: [paper.view.center.x, paper.view.center.y + this.sizes.circle / 2]
                 }
             }),
-            circle: new paper.Path.Rectangle({
+            fill: new paper.Path.Rectangle({
                 center: [paper.view.center.x, paper.view.center.y],
                 // radius: this.sizes.circle / 2,
                 size: [this.sizes.triangle, this.sizes.triangle],
@@ -24930,15 +24930,15 @@ Morph.prototype._initContent = function() {
         raster: [
             new paper.Raster({
                 source: '/img/canvas1.png',
-                position: [paper.view.center.x, paper.view.center.y]
+                position: [paper.view.center.x, paper.view.center.y + 300]
             }),
             // new paper.Raster({
             //     source: '/img/canvas2.png',
-            //     position: paper.view.center
+            //     position: [paper.view.center.x, paper.view.center.y + 300]
             // }),
             // new paper.Raster({
             //     source: '/img/canvas3.png',
-            //     position: paper.view.center
+            //     position: [paper.view.center.x, paper.view.center.y + 300]
             // })
         ]
     };
@@ -24961,6 +24961,8 @@ Morph.prototype._initContent = function() {
         children: children,
         clipped: true
     });
+
+    console.log(this.group);
 };
 
 Morph.prototype._initEvents = function() {
@@ -24969,13 +24971,13 @@ Morph.prototype._initEvents = function() {
 
     this.group.onClick = function() {
         if ( clickCount === 0 ) {
-            that.circle();
-        }
-        if ( clickCount === 1 ) {
             that.square();
         }
-        if ( clickCount === 2 ) {
+        if ( clickCount === 1 ) {
             that.triangle();
+        }
+        if ( clickCount === 2 ) {
+            that.circle();
             clickCount = 0;
             return;
         }
@@ -25012,18 +25014,30 @@ Morph.prototype._calcPoints = function() {
             {x:                      0, y: -this.sizes.circle / 2},
             {x:  this.sizes.circle / 2, y:                      0},
             {x:                      0, y:  this.sizes.circle / 2}
+            // {x: -255, y:    0},
+            // {x:    0, y: -255},
+            // {x:  255, y:    0},
+            // {x:    0, y:  255},
         ],
         square: [
             {x: -this.sizes.square / 2, y: -this.sizes.square / 2},
             {x:  this.sizes.square / 2, y: -this.sizes.square / 2},
             {x:  this.sizes.square / 2, y:  this.sizes.square / 2},
             {x: -this.sizes.square / 2, y:  this.sizes.square / 2}
+            // {x: -225, y: -225},
+            // {x:  225, y: -225},
+            // {x:  225, y:  225},
+            // {x: -225, y:  225},
         ],
         triangle: [
-            {x: -0.59655 * this.sizes.triangle / 2, y: 0.55172 * this.sizes.triangle / 2},
+            {x: -0.36207 * this.sizes.triangle / 2, y: 0.58621 * this.sizes.triangle / 2},
             {x:           -this.sizes.triangle / 2, y: 0.39310 * this.sizes.triangle / 2},
             {x:  0.53448 * this.sizes.triangle / 2, y:          -this.sizes.triangle / 2},
             {x:            this.sizes.triangle / 2, y:           this.sizes.triangle / 2}
+            // {x: -105, y:  170},
+            // {x: -290, y:  114},
+            // {x:  155, y: -290},
+            // {x:  290, y:  290},
         ]
     };
 };
@@ -25033,52 +25047,85 @@ Morph.prototype.update = function() {
 };
 
 Morph.prototype._morph = function(state) {
+
+    var prevState  = this._getState();
+
+    if ( state == prevState ) return;
+
+    console.log('%c' + prevState + ' => ' + state, 'background:yellow');
+
     var vertexes   = [],
         vectorArr  = [],
         callCount  = this.dur * 60 / 1000, // morph anim duration * frame per second
-        path       = this.objects.paths.morph,
-        segments   = path.segments,
-        prevState  = this._getState(),
-        prevPoints = this.point[prevState],
-        points     = this.point[state];
-
-    if ( state == prevState ) return;
+        morphPath  = this.objects.paths.morph,
+        segments   = morphPath.segments,
+        prevPoints = this._translateCoordinates(this.point[prevState]),
+        points     = this._translateCoordinates(this.point[state]);
 
     $.each(segments, function(index, segment) {
         vertexes.push(segment.point);
     });
 
-    $.each(points, function(index, point) {
-        console.log(index);
-        var x = vertexes[index].x - paper.view.center.x - point.x;
-        var y = vertexes[index].y - paper.view.center.y - point.y;
+    for (var i = 0; i < points.length; i++) {
+        var x = prevPoints[i].x - points[i].x;
+        var y = prevPoints[i].y - points[i].y;
         vectorArr.push({x: x, y: y});
-    });
+    }
 
-    path.onFrame = function(event) {
+    morphPath.onFrame = function(event) {
+        var i;
 
-        $.each(vertexes, function(index, vertex) {
-            vertex.x += vectorArr[index].x / callCount;
-            vertex.y += vectorArr[index].y / callCount;
-        });
+        if ( event.count < callCount ) {
+            for ( i = 0; i < vertexes.length; i++ ) {
+                vertexes[i].x -= vectorArr[i].x / callCount;
+                vertexes[i].y -= vectorArr[i].y / callCount;
+            }
 
-        segments[0].handleIn.y = segments[1].handleOut.x =
-        segments[2].handleOut.y = segments[3].handleIn.x -= 140 / callCount;
-        segments[0].handleOut.y = segments[1].handleIn.x =
-        segments[2].handleIn.y = segments[3].handleOut.x += 140 / callCount;
+            if ( prevState == 'circle' ) {
+                segments[0].handleIn.y = segments[1].handleOut.x =
+                segments[2].handleOut.y = segments[3].handleIn.x -= 140 / callCount;
+                segments[0].handleOut.y = segments[1].handleIn.x =
+                segments[2].handleIn.y = segments[3].handleOut.x += 140 / callCount;
+            }
 
-        if ( event.count == callCount ) path.onFrame = undefined;
+            if ( state == 'circle' ) {
+                segments[0].handleIn.y = segments[1].handleOut.x =
+                segments[2].handleOut.y = segments[3].handleIn.x += 140 / callCount;
+                segments[0].handleOut.y = segments[1].handleIn.x =
+                segments[2].handleIn.y = segments[3].handleOut.x -= 140 / callCount;
+            }
+        }
+        else {
+            for ( i = 0; i < vertexes.length; i++ ) {
+                vertexes[i].x = points[i].x;
+                vertexes[i].y = points[i].y;
+            }
+
+            if ( prevState == 'circle' ) {
+                segments[0].handleIn.y = segments[1].handleOut.x =
+                segments[2].handleOut.y = segments[3].handleIn.x = 0;
+                segments[0].handleOut.y = segments[1].handleIn.x =
+                segments[2].handleIn.y = segments[3].handleOut.x = 0;
+            }
+
+            if ( state == 'circle' ) {
+                segments[0].handleIn.y = segments[1].handleOut.x =
+                segments[2].handleOut.y = segments[3].handleIn.x = 140;
+                segments[0].handleOut.y = segments[1].handleIn.x =
+                segments[2].handleIn.y = segments[3].handleOut.x = -140;
+            }
+        }
+
+        if ( event.count == callCount ) morphPath.onFrame = undefined;
     };
 
     this._updateState(state);
 
-    var _ = this;
-
     // console.log(path);
-    // console.log(segments);
-    console.log(vertexes);
+    console.log(segments);
+    // console.log(vertexes);
     console.log(vectorArr);
-    console.log(_);
+    console.log(points);
 };
 
 Morph.prototype.square = function() {
@@ -25099,6 +25146,16 @@ Morph.prototype._updateState = function(state) {
 
 Morph.prototype._getState = function() {
     return this.state;
+};
+
+Morph.prototype._translateCoordinates = function(pointsArray) {
+    var arr = [];
+    $.each(pointsArray, function(index, value) {
+        var x = value.x + paper.view.center.x;
+        var y = value.y + paper.view.center.y;
+        arr.push({x: x, y: y});
+    });
+    return arr;
 };
 
 
